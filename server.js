ca436c748ba5439694db116381a076b1454dd133
Mathis Latimier <mathis.latimier@gmail.com>
2026-07-06 13:40:17 +0200

    Etape 2: ajout de dotenv

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const session = require('express-session')
 const authRoutes = require('./routes/auth')
 const adminRoutes = require('./routes/admin')
 
@@ -7,12 +9,24 @@ const app = express()
 app.use(express.json())
 app.use(express.static('public'))
 
+app.use(session({
+  name: 'bat_identity',
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    sameSite: true,
+    maxAge: 1800000
+  }
+}))
+
 // Routes
 app.use('/auth', authRoutes)
 app.use(adminRoutes)
 
-// Lance le serveur en local, sur le port 3000
-const PORT = 3000
+// Lance le serveur en local
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`)
 })
