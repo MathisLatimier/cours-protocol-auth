05abfc0223019fda38825bec1f46ef2911c2f53c
Mathis Latimier <mathis.latimier@gmail.com>
2026-07-22 08:40:09 +0200

    ÉTAPE 1 : INJECTION D’HELMET

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth')
 const adminRoutes = require('./routes/admin')
 
 const app = express()
 
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
